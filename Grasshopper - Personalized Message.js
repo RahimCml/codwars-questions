cc50097b40258ebcb92575b1f66aa4d3030d6d87
RehimRay <rhmcml@gmail.ru>
2021-02-20 19:05:30 +0400

    Grasshopper - Personalized Message

---
+++ Grasshopper - Personalized Message.js
@@ -0,0 +1,29 @@
+// Grasshopper - Personalized Message
+
+// Personalized greeting
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+
+
+function greet (name, owner) {
+  if (name === owner) {
+    return 'Hello boss';
+  }
+  return 'Hello guest';
+}
+
+
+//////another solution
+
+function greet (name, owner) {
+  return `Hello ${name==owner?'boss':'guest'}`
+}
+//////another solution
+
+const greet = (name, owner) => name == owner? 'Hello boss': 'Hello guest';
